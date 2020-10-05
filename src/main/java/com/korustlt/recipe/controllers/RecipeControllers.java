bd1c7216f668fae65861d358719f930ded191938
@kostyaFrom <korustlt@gmail.com>
2020-10-05 19:34:28 +0400

    setup show.html page

---
+++ src/main/java/com/korustlt/recipe/controllers/RecipeControllers.java
@@ -0,0 +1,23 @@
+package com.korustlt.recipe.controllers;
+
+import com.korustlt.recipe.service.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeControllers {
+
+    private final RecipeService recipeService;
+
+    public RecipeControllers(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/show";
+    }
+}
--- src/test/java/com/korustlt/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/korustlt/recipe/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,46 +17,57 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
-
-    IndexController indexController;
-
-    @Mock
-    Model model;
 
     @Mock
     RecipeService recipeService;
 
+    @Mock
+    Model model;
+
+    IndexController controller;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        indexController = new IndexController(recipeService);
+
+        controller = new IndexController(recipeService);
     }
 
-//    @Test
-//    public void testMockMvc() {
-//        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController)
-//                .setViewResolvers()
-//                .build();
-//        mockMvc.perform(get("/"))
-//        .andExpect(status().isOk())
-//        .andExpect(view().name("index"));
-//    }
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
 
     @Test
-    public void getIndexPage() {
+    public void getIndexPage() throws Exception {
+
+        //given
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(new Recipe());
+
         Recipe recipe = new Recipe();
         recipe.setId(1L);
+
         recipes.add(recipe);
 
         when(recipeService.getRecipes()).thenReturn(recipes);
 
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
-        String viewName = indexController.getIndexPage(model);
+        //when
+        String viewName = controller.getIndexPage(model);
+
+
+        //then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
