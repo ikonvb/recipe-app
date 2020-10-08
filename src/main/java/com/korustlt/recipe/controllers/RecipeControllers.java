a9318619fdbc3e21390d0c2697ae5f4fff7d69c4
@kostyaFrom <korustlt@gmail.com>
2020-10-08 18:25:50 +0400

    add recipeform.html

--- src/main/java/com/korustlt/recipe/controllers/RecipeControllers.java
+++ src/main/java/com/korustlt/recipe/controllers/RecipeControllers.java
@@ -1,9 +1,12 @@
 package com.korustlt.recipe.controllers;
 
+import com.korustlt.recipe.commands.RecipeCommand;
 import com.korustlt.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,18 @@ public class RecipeControllers {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
